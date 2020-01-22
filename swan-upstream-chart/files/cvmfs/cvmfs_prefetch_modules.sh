de22666702d6647e91cab795c1255ced30a32285
Piotr Mrowczynski <piotr.mrowczynski@cern.ch>
2020-01-22 16:28:59 +0100

    Fix cvmfs prefetch when cvmfs not available

--- swan-upstream-chart/files/cvmfs/cvmfs_prefetch_modules.sh
+++ swan-upstream-chart/files/cvmfs/cvmfs_prefetch_modules.sh
@@ -10,16 +10,23 @@ log_error() {
 log_info "Started cvmfs lcg prefetching.."
 
 while true; do
-    source /cvmfs/sft.cern.ch/lcg/views/LCG_96/x86_64-centos7-gcc8-opt/setup.sh && ( timeout 10s python -m ipykernel > /dev/null 2>&1 || true )
-    if [ $? -ne 0 ]; then
-        log_error "Getting ipykernel failed"
-        exit 1
-    fi
+    LCG_96_SETUP="/cvmfs/sft.cern.ch/lcg/views/LCG_96/x86_64-centos7-gcc8-opt/setup.sh"
+    if [ -f "$LCG_96_SETUP" ]; then
+        # Get ipykernel in subshell and get return code
+        (source $LCG_96_SETUP && (timeout 10s python -m ipykernel > /dev/null 2>&1 || true ))
+        if [ $? -ne 0 ]; then
+            log_error "Getting ipykernel failed"
+            exit 1
+        fi
 
-    source /cvmfs/sft.cern.ch/lcg/views/LCG_96/x86_64-centos7-gcc8-opt/setup.sh && ( timeout 20s python -m JupyROOT.kernel.rootkernel > /dev/null 2>&1 || true )
-    if [ $? -ne 0 ]; then
-        log_error "Getting JupyROOT.kernel.rootkernel failed"
-        exit 1
+        # Get JupyROOT.kernel.rootkernel in subshell and get return code
+        (source $LCG_96_SETUP && ( timeout 20s python -m JupyROOT.kernel.rootkernel > /dev/null 2>&1 || true ))
+        if [ $? -ne 0 ]; then
+            log_error "Getting JupyROOT.kernel.rootkernel failed"
+            exit 1
+        fi
+    else
+        log_error "Sourcing $LCG_96_SETUP failed, path not accessible"
     fi
 
     sleep 15m
