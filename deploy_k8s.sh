65e41dd170698235ed777413d37d0b8e39dbf588
Piotr Mrowczynski <piotr.mrowczynski@cern.ch>
2020-01-30 11:39:31 +0100

    update deploy_k8s with upgrade db

--- deploy_k8s.sh
+++ deploy_k8s.sh
@@ -2,11 +2,21 @@ set -e
 
 ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; pwd;)
 
-SWAN_ENV=$1
+echo "# USAGE: ${ROOT_DIR}/deploy_k8s.sh --env <qa/prod> (--upgrade-db)"
+echo ""
+
+SWAN_ENV=""
+UPGRADE_DB="false"
+
+while [[ "$#" -gt 0 ]]; do case $1 in
+  --env) SWAN_ENV="$2"; shift;;
+  --upgrade-db) UPGRADE_DB="true";;
+  *) echo "Unknown parameter passed: $1"; exit 1;;
+esac; shift; done
 
 AVAILABLE_SWAN_ENV=" prod qa "
-if ! [[ -n "${1}" && " ${AVAILABLE_SWAN_ENV[@]} " =~ " ${1} " ]]; then
-    echo "ERROR: ${1} is not available swan deployment environment"
+if ! [[ -n "${SWAN_ENV}" && " ${AVAILABLE_SWAN_ENV[@]} " =~ " ${SWAN_ENV} " ]]; then
+    echo "ERROR: ${SWAN_ENV} is not available swan deployment environment"
 	exit 1
 fi
 
@@ -20,6 +30,7 @@ if [[ $SWAN_ENV == "prod" && $(git symbolic-ref -q HEAD) != "refs/heads/master" 
     echo "ERROR: Cannot deploy prod environment on branch other than master"
     exit 1
 fi
+
 
 # kubernetes access based on environment
 export KUBECONFIG=/srv/swan-k8s/private/swan.$SWAN_ENV.kubeconfig
@@ -37,19 +48,47 @@ HADOOP_AUTH_KEYTAB_ENCODED=$(base64 -w 0 $HADOOP_AUTH_KEYTAB_PATH)
 SPARKK8S_AUTH_TOKEN_PATH=/srv/swan-k8s/private/sparkk8s.kubeconfig
 SPARKK8S_AUTH_TOKEN_ENCODED=$(base64 -w 0 $SPARKK8S_AUTH_TOKEN_PATH)
 
+echo ""
+echo "Updating eosxd"
+echo ""
+
 helm upgrade --install --namespace kube-system  \
 eosxd $ROOT_DIR/swan-eosxd-config-chart
+
+if [[ $? -ne 0 ]]
+then
+    echo "failed"
+    exit 1
+fi
+
+echo ""
+echo "Updating fluentd"
+echo ""
 
 helm upgrade --install --namespace kube-system  \
 fluentd $ROOT_DIR/swan-fluentd-config-chart
 
+if [[ $? -ne 0 ]]
+then
+    echo "failed"
+    exit 1
+fi
+
+echo ""
+echo "Updating swan env ${SWAN_ENV}, upgrade db ${UPGRADE_DB}"
+echo ""
+
 helm upgrade --install --namespace swan  \
 --values $SWAN_PROD_VALUES_PATH \
 --values $SWAN_SECRET_VALUES_PATH \
---set jupyterhub.hub.annotations.version="release-$(date +%s)" \
+--set jupyterhub.hub.db.upgrade=$UPGRADE_DB \
 --set swan.secrets.hadoop.cred=$HADOOP_AUTH_KEYTAB_ENCODED \
 --set swan.secrets.eos.cred=$EOS_AUTH_KEYTAB_ENCODED \
 --set swan.secrets.sparkk8s.cred=$SPARKK8S_AUTH_TOKEN_ENCODED \
 $SWAN_PROD_RELEASE_NAME $ROOT_DIR/swan-upstream-chart
 
-
+if [[ $? -ne 0 ]]
+then
+    echo "failed"
+    exit 1
+fi
