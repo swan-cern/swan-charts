bc1fa02d91b4e7709c1dd4b6abe3c6d27d6e40cd
rccern <70897679+rccern@users.noreply.github.com>
2021-11-03 08:07:25 +0100

    remove optional hive delegation token to align with puppet (#42)

--- swan-cern/files/private/prod/hadoop_token.sh
+++ swan-cern/files/private/prod/hadoop_token.sh
@@ -17,7 +17,7 @@ export OVERRIDE_HADOOP_MAPRED_HOME=$LCG_VIEW
 source $LCG_VIEW/setup.sh
 source /cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/hadoop-swan-setconf.sh $CLUSTER
 kinit -V -kt /srv/jupyterhub/private/hadoop.cred hswan@CERN.CH -c $KRB5CCNAME >/dev/null 2>&1
-/usr/hdp/hadoop-fetchdt-0.2.0/hadoop-fetchdt -required hdfs,yarn -optional hive -proxyuser $USER -tokenfile /tmp/hadoop_$USER >/dev/null 2>&1
+/usr/hdp/hadoop-fetchdt-0.2.0/hadoop-fetchdt -required hdfs,yarn -proxyuser $USER -tokenfile /tmp/hadoop_$USER >/dev/null 2>&1
 echo $(cat /tmp/hadoop_$USER | base64 -w 0)
 
 kdestroy -c $KRB5CCNAME
