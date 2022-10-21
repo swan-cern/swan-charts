d2220f2ae2bef442181cfc9335a22177d05da0b2
Krishnan R <krishnanr1997@gmail.com>
2022-10-21 16:46:23 +0200

    Address review comments

--- .github/workflows/list-changed-charts.sh
+++ .github/workflows/list-changed-charts.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# Prints a line "${CHART_NAME}_has_changed=<true|false>" for every chart (swan, swan-cern) that has changed since last tag ${CHART_NAME}@x.y.z
 
 set -euxo pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" || exit; pwd;)"
 
-cd ${script_dir}/../../ # cd to the root of the repo
+cd "${script_dir}/../../" # cd to the root of the repo
 
 CHARTS=("swan" "swan-cern")
 
@@ -13,9 +14,9 @@ for CHART_NAME in "${CHARTS[@]}"; do
     LAST_CHART_TAG="$(git tag --list --sort -version:refname ${CHART_NAME}@* | head -n 1)"
 
     # Determine if there are any changes on the chart/ folder since last tag
-    HAS_CHANGED="$(git diff --quiet HEAD ${LAST_CHART_TAG} -- ./${CHART_NAME} || echo true)"
-
-    # print output
-    echo "${CHART_NAME}_has_changed=${HAS_CHANGED}"
-    
+    if git diff --quiet HEAD "${LAST_CHART_TAG}" -- ./"${CHART_NAME}"; then
+         echo "${CHART_NAME}_has_changed=false" # git diff exited with 0, there are no changes
+    else
+         echo "${CHART_NAME}_has_changed=true" # git diff exited with 1, there are changes
+    fi
 done
