04bbb257d8fcd709176b2dff401a245a1df2f6fa
Rodrigo Sobral <54753829+RodrigoSobral2000@users.noreply.github.com>
2024-07-12 15:34:02 +0100

    Prevent KeyErrors due to introduction of custom environments
    The modified code was initially projected to be dependent on LCG configurations. With the introduction of custom software environments, some options might not be set and protections need to be taken against it.

--- swan-cern/files/swan_computing_config.py
+++ swan-cern/files/swan_computing_config.py
@@ -139,9 +139,6 @@ class SwanComputingPodHookHandler(SwanPodHookHandlerProd):
 
         cluster = self.spawner.user_options[self.spawner.spark_cluster_field]
 
-        if cluster == 'none':
-            return None
-
         username = self.spawner.user.name
         hadoop_secret_name = f'hadoop-tokens-{username}'
 
@@ -275,7 +272,7 @@ class SwanComputingPodHookHandler(SwanPodHookHandlerProd):
         """
         return True if the user has requested a GPU
         """
-        return "cu" in self.spawner.user_options[self.spawner.lcg_rel_field]
+        return "cu" in self.spawner.user_options.get(self.spawner.lcg_rel_field, '')
 
     def _spark_enabled(self):
         """
@@ -286,7 +283,7 @@ class SwanComputingPodHookHandler(SwanPodHookHandlerProd):
         """
 
         user_roles = self.spawner.user_roles
-        cluster = self.spawner.user_options[self.spawner.spark_cluster_field]
+        cluster = self.spawner.user_options.get(self.spawner.spark_cluster_field, 'none')
 
         if cluster == "analytix" and "analytix" not in user_roles:
            raise ValueError(
@@ -314,9 +311,6 @@ class SwanComputingPodHookHandler(SwanPodHookHandlerProd):
 
         cluster = self.spawner.user_options[self.spawner.spark_cluster_field]
         max_mem = self.spawner.user_options[self.spawner.user_memory]
-
-        if cluster == 'none':
-            return
 
         # add basic spark envs
 
@@ -376,7 +370,7 @@ class SwanComputingPodHookHandler(SwanPodHookHandlerProd):
         """
         return True if the user has selected an HTCondor pool.
         """
-        condor_pool = self.spawner.user_options[self.spawner.condor_pool]
+        condor_pool = self.spawner.user_options.get(self.spawner.condor_pool, 'none')
         return condor_pool != 'none'
 
     async def _open_ports(self, num_ports):
