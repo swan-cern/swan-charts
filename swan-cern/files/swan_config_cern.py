65873d46f7b9043124825f2bd0d82a77e7fd8767
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2024-11-26 11:51:51 +0100

    [swan-cern] Use image hosted in gitlab for side container

--- swan-cern/files/swan_config_cern.py
+++ swan-cern/files/swan_config_cern.py
@@ -193,7 +193,7 @@ class SwanPodHookHandlerProd(SwanPodHookHandler):
         pod_spec_containers.append(
             V1Container(
                 name='side-container',
-                image='cern/cc7-base:20181210',
+                image='gitlab-registry.cern.ch/linuxsupport/cc7-base:20230801-1.x86_64',
                 command=['/srv/side-container/side_container_tokens_perm.sh'],
                 args=[
                     env['USER_ID'],
